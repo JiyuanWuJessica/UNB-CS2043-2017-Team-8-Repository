aa8c79e774338505a4138557c5c780ed835a50ad
Jess_W <Jess_W@192.168.2.20>
2017-11-30 21:36:10 -0400

    Jessica updated.

---
+++ CS2043Lab9/src/InfoApp.java
@@ -0,0 +1,11 @@
+
+public class InfoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InfoPrintWu wu = new InfoPrintWu();;
+		System.out.println("Team x member list:");
+		System.out.println(wu.printWu());
+	}
+
+}
---
+++ CS2043Lab9/src/InfoPrintWu.java
@@ -0,0 +1,7 @@
+
+public class InfoPrintWu {
+	// TODO Auto-generated method stub
+	public String printWu() {
+		return "Jessica (Jiyuan) Wu";
+	}
+}
